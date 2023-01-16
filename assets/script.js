a75a7c52765f1e91e362e8e65ebdd1138bd036d3
Tyler <hilltyler49@gmail.com>
2023-01-16 01:24:00 -0600

    completed pseudocode

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,30 @@
-const weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=049980ce88447ba2a5760c6844c0482d";
-const cityWeatherAPI = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+const apiKey = "049980ce88447ba2a5760c6844c0482d";
+const weatherApiUrl = "api.openweathermap.org/data/2.5/forecast?id={city ID}&appid={API key}";
+// want to use both APIs, as the city one gives the lat and long for the weather one
+const searchBtn = document.querySelector("#searchBtn");
+
+// global variables
+// divs
+
+// functions
+function init() {
+  // grab last search results from local storage and put them on page
+  // use code from challenge 4 or 5
+}
+
+function citySearch() {
+  fetch(weatherApiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+// calls + event listeners
+init();
+
+// search button event listener
+searchBtn.addEventListener("click", citySearch);
+// past search event listener
