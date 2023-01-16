37a6fe15be1b9cdab95494858db86f4f8fbf4f85
Tyler <hilltyler49@gmail.com>
2023-01-16 00:48:41 -0600

    began pseudo-coding

---
+++ assets/script.js
@@ -0,0 +1,2 @@
+const weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=049980ce88447ba2a5760c6844c0482d";
+const cityWeatherAPI = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
