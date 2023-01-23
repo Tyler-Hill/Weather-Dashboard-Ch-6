d3dab7ae296d8b3a665bb51be2ae224125cef16f
Tyler <hilltyler49@gmail.com>
2023-01-22 22:51:17 -0600

    implemented init function

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,5 @@
 const apiKey = "049980ce88447ba2a5760c6844c0482d";
 // global variables
-const weatherApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Chicago&units=imperial&appid=049980ce88447ba2a5760c6844c0482d";
-// const sixteenDayApi = "api.openweathermap.org/data/2.5/forecast/daily?q=Chicago&cnt=6&appid=049980ce88447ba2a5760c6844c0482d";
-const weatherApiImages = "https://openweathermap.org/img/wn/01d@2x.png";
-// make it look like this https://openweathermap.org/img/wn/${data.list[0].weather.icon}@2x.png
 // divs
 const searchBtn = document.querySelector("#searchBtn");
 const pastSearches = document.querySelector("#pastSearches");
@@ -15,47 +11,61 @@ const city_Temp = document.querySelector("#city-Temp");
 const city_Wind = document.querySelector("#city-Wind");
 const city_Humidity = document.querySelector("#city-Humidity");
 const city_Cards = document.querySelector("#city-Cards");
-const cityArray = [];
+const searchBar = document.querySelector("#cityInput");
+let cityArray = [];
+let city = "";
 // date
-let d = new Date();
-let localTime = d.getTime();
-let localOffset = d.getTimezoneOffset() * 60000;
-let utc = localTime + localOffset;
-let atlanta = utc + 1000 * -14400;
-let nd = new Date(atlanta);
+// let d = new Date();
+// let localTime = d.getTime();
+// let localOffset = d.getTimezoneOffset() * 60000;
+// let utc = localTime + localOffset;
+// let atlanta = utc + 1000 * -14400;
+// let nd = new Date(atlanta);
 
 // functions
+// Init function
 function init() {
+  let initCities = JSON.parse(localStorage.getItem("cityArray"));
+  if (initCities !== null) {
+    cityArray = initCities;
+  }
+  city = cityArray[0];
+  citySearch();
   // grab last search results from local storage and put them on page
-  // use code from challenge 4 or 5
 }
 
-function citySearch(event) {
+// Look for new city
+function newSearch(event) {
   event.preventDefault();
-  let city = document.querySelector("#cityInput").value;
-  cityArray.push(city);
-  localStorage.setItem("Searched Cities", cityArray);
+  city = searchBar.value;
+  cityArray.unshift(city);
+  localStorage.setItem("cityArray", JSON.stringify(cityArray));
+
+  citySearch();
+}
+
+function searchHistory() {}
+
+// Main fetch Function
+function citySearch() {
   const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=049980ce88447ba2a5760c6844c0482d`;
   fetch(weatherApiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       city_Name.innerHTML = data.city.name;
-      // city_Date.innerHTML =
       let weatherImg = data.list[0].weather[0].icon;
       city_Weather.setAttribute("src", `https://openweathermap.org/img/wn/${weatherImg}@2x.png`);
       city_Temp.innerHTML = `Temperature: ${data.list[0].main.temp}°F`;
       city_Wind.innerHTML = `Wind Speed: ${data.list[0].wind.speed} MPH`;
       city_Humidity.innerHTML = `Humidity: ${data.list[0].main.humidity}%`;
 
-      // City name date weather conditions, temperature, humidity, wind speed
-      // data goes in here, make a loop function or summin
+      city_Cards.innerHTML = "";
       let index = 1;
       for (const element of data.list) {
         let dateText = element.dt_txt.split(" ")[1];
-        // console.log(dateText);
+        // Consider re-formatting of date
         let weatherCardImg = element.weather[0].icon;
         if (dateText === "00:00:00") {
           // element creation
@@ -99,7 +109,5 @@ function citySearch(event) {
 init();
 
 // search button event listener
-searchBtn.addEventListener("click", citySearch);
+searchBtn.addEventListener("click", newSearch);
 // past search event listener
-
-// 86400 seconds in 24 hours
